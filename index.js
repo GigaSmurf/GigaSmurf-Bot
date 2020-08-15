6e28832eeff76e2e482e1f7d2155b162ded86382
GigaSmurf <nalyd.putra@gmail.com>
2020-08-15 00:16:09 -0500

    asdsa

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-
+const puppetteer = require('puppeteer');
 
 //initialize firebase firestore
 var admin = require("firebase-admin");
@@ -119,7 +119,79 @@ bot.on('message',async message => {
 
   switch (args[0].toLowerCase()){
     case 'live':
-     bot.commands.get('live').execute(message,args);
+    //  bot.commands.get('live').execute(message,args);
+    let name1 = args.slice(1).join("+");
+      let display = args.slice(1).join(" ");
+        let person = message.author.id;
+      try{
+        const browser = await puppetteer.launch({
+          args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+          ],
+        });
+          const page = await browser.newPage();
+          // const navigationPromise = page.waitForNavigation({waitUntil: "domcontentloaded"});
+        //w 1920, h 1200 
+        await page.setViewport({ width: 1920, height: 1200, deviceScaleFactor: 1 });
+          await page.goto(`https://na.op.gg/summoner/userName=${name1}`);
+        
+          
+            await page.waitForSelector('.SpectateTabButton');
+        
+            const button = await page.$('.SpectateTabButton');
+            button.click();
+          // await navigationPromise;
+          
+          await page.waitForSelector('.SpectateSummoner');
+        //   console.log("Loading Success");
+        
+          const shot = await page.$('.SpectateSummoner')
+          const box = await shot.boundingBox();
+        
+          const x1 = box.x;                                // coordinate x
+          const y1 = box.y;                                // coordinate y
+          const w1 = box.width;                            // area width
+          const h1 = box.height;                           // area height
+          console.log(`${x1}, ${y1}, ${w1}, ${h1}`);
+
+        //   Takes screenshot
+           await page.screenshot({path: `images/${person}.png`, clip: {x: x1, y: y1, width: w1, height: h1}, omitBackground: true });
+          // await page.screenshot({'path': 'images/esports.png', 'clip': {'x': x, 'y': y, 'width': w, 'height': h}});  
+          console.log("screenshot taken");
+        //   Embed
+          const liveshot = new Discord.MessageEmbed()
+          .setColor('#5383e9')
+          .setTitle(`${display}'s Live Game`)
+          .setURL(`https://na.op.gg/summoner/userName=${name1}`)
+          .setDescription("")
+          .attachFiles([`./images/${person}.png`])
+          .setImage(`attachment://${person}.png`);
+    
+          await message.channel.send(liveshot);
+        // await message.channel.send(`Here is the summoner ${display}`, {files: ["./images/esports.png"]});
+        console.log("Embed works");
+        // Closes the browser
+          await browser.close();
+
+          }
+        //   Catch Error
+          catch(e){
+            console.log('This error: ', e)
+            message.channel.send("This player is currently not in a game.");
+          }
+
+        //   Deletes Image after try catch in case error after saving the image
+            const path = `./images/${person}.png`;
+
+        fs.unlink(path, (err) => {
+          if (err) {
+            console.error(err)
+            return
+          }
+        
+          //file removed
+        })
       break;
     case 'prefix':
       // Changes the prefix
